40c87c26d8bba40deb997d1da61fb960c2b977e7
alexvcs <alexcurtisslep@gmail.com>
2023-10-17 10:30:13 -0400

    get edit functionality working

--- src/List.js
+++ src/List.js
@@ -38,79 +38,55 @@ export default function List({ newItem, setNewItem }) {
     setList(listCopy);
   };
 
-  const handleEdit = (e, listItem) => {
-    // listItem.edit === "true";
-    console.log("listItem.edit", listItem.edit);
-    console.log("this is e.target.value", e.target.value)
-    // setList(listCopy);
-    // need index of item
-    // edit task to be new value
-    // update array with new object
-    // change edit boolean
-
+  const handleEdit = (e, index) => {
+    const updatedTask = e.target.value
+    const addUpdatedTask = {...list[index], task: `${updatedTask}`}
+    listCopy[index] = addUpdatedTask
+    setList(listCopy)
   };
 
-  const editListItem = (listItem) => {
-    // let itemIndex = listCopy.findIndex(listItem)
-    // console.log('listItem', listItem)
-
-    // const index = list.findIndex((listItem));
-
-    // console.log('index', index)
-
-    // listCopy[index].edit = "true";
-
-    const newArr = list.map((listItem) => {
-      if (listItem.id === listItem) {
-        return {...listItem, edit: true};
-      }
-
-    setList(listCopy);
-    });
-    console.log('list', list)
-
-    
-    // listItem.edit === true;
-    // listCopy.forEach((listItem) => {
-    //   listItem.edit = true;
-    // }); 
-
-    // const editTrue = { edit: true }
-
-    // listCopy.map((listItem) => 
-    //   listItem.id === listItem.id ? {...listItem, ...editTrue} : listItem
-    // );
-    // setList(listCopy)
-    // console.log('this runs')
-    // console.log('listItem2', listItem)
-    // console.log('this is the index', listItem.id);
-    // setEditItem(!editItem);
-    // setEditButton('Done');
-    // this update should set the list to the updated item after editing
-    // const update = listCopy.map((listItem) =>
-    //   listItem.id === updateItem.id ? updateItem : listItem
-    // );
-    // setList(update);
+  const editListItem = (index) => {
+    const makeItemEditTrue = {...list[index], edit: true };
+    listCopy[index] = makeItemEditTrue;
+    setList(listCopy)
   };
 
-  const toDoList = list.map((listItem) => {
-    // console.log('this is the list', list);
+  const updateListItem = (index) => {
+     const makeItemEditFalse = {...list[index], edit: false};
+     listCopy[index] = makeItemEditFalse;
+     setList(listCopy);
+  }
+
+  const toDoList = list.map((listItem, index) => {
     return (
       <div key={listItem.id}>
         {listItem.edit === false ? (
-          <span>{listItem.task}</span>
+          <>
+            <span>{listItem.task}</span>
+            <button
+              onClick={() => editListItem(index)}
+              className="editButton"
+            >
+              Edit
+            </button>
+          </>
         ) : (
-          <input
-            // listItem being passed in as a parameter to onChange makes its value undefined
-            // onChange={(e, listItem) => handleEdit(e, listItem)}
-            onChange={(e) => handleEdit(e, listItem)}
-            value={listItem.task}
-          />
+          <>
+            <input
+              // listItem being passed in as a parameter to onChange makes its value undefined
+              // onChange={(e, listItem) => handleEdit(e, listItem)}
+              onChange={(e) => handleEdit(e, index)}
+              value={listItem.task}
+            />
+            <button
+              onClick={() => updateListItem(index)}
+              className="editButton"
+            >
+              Update
+            </button>
+          </>
         )}
 
-        <button onClick={() => editListItem(listItem)} className="editButton">
-          Edit
-        </button>
         <button onClick={() => removeFromList(listItem)}>Delete</button>
       </div>
     );

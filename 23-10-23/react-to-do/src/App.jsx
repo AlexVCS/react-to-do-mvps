26341a077b803753ef93604170f3ea3b364ea61f
alexvcs <alexcurtisslep@gmail.com>
2023-10-25 14:38:20 -0400

    adding base components for the project

--- 23-10-23/react-to-do/src/App.jsx
+++ 23-10-23/react-to-do/src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
+import AddToDo from "./AddToDo";
+import List from "./List"
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setToDos] = useState([])
 
-  return <div className="text-3xl underline">Content will be added here</div>;
+  return <>
+  <h1 className="text-2xl">To Do</h1>
+  <AddToDo />
+  <List toDos={toDos} />
+  </>;
 }
 
 export default App
---
+++ 23-10-23/react-to-do/src/List.jsx
@@ -0,0 +1,8 @@
+
+const List = (props) => {
+  return (
+    <div>To do list will go here</div>
+  )
+}
+
+export default List

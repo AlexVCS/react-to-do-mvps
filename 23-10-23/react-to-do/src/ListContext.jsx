94e2d7899baaade18925dda9f00317ae972edbc1
alexvcs <alexcurtisslep@gmail.com>
2023-11-01 19:33:56 -0400

    implementing tasksReducer function in ListContext.jsx

--- 23-10-23/react-to-do/src/ListContext.jsx
+++ 23-10-23/react-to-do/src/ListContext.jsx
@@ -20,3 +20,38 @@ export function ListProvider({ children }) {
     </TasksContext.Provider>
   );
 }
+
+export function useTasks() {
+  return useContext(TasksContext)
+}
+
+export function useTasksDispatch() {
+  return useContext(TasksDispatchContext)
+}
+
+function tasksReducer(tasks, action) {
+  switch(action.type) {
+    case: 'added': {
+      return [...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false
+      }]
+    }
+    case: 'changed': {
+      return tasks.map(t => {
+        if(t.id === action.task.id) {
+          return action.task
+        } else {
+          return t;
+        }
+      })
+    }
+    case: 'deleted': {
+      return tasks.filter(t => t.id !== action.id)
+    }
+    default: {
+      throw Error('Unknown action ' + action.type)
+    }
+  }
+}
